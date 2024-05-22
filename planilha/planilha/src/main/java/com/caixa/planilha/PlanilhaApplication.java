5e631be3decfdbb4bc59cfedb7f03da16a65efee
Vinicius de Oliveira <viniciusmgomes2005@hotmail.com>
2024-05-22 07:07:30 -0300

    startando projeto de finanças

---
+++ planilha/planilha/src/main/java/com/caixa/planilha/PlanilhaApplication.java
@@ -0,0 +1,13 @@
+package com.caixa.planilha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanilhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanilhaApplication.class, args);
+	}
+
+}
---
+++ visao/financeiro/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+    import { RouterLink } from 'vue-router';
+    export default {
+        name: 'HeaderComponent',
+        components: {
+            RouterLink
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <h1></h1>
+        <nav>
+            <router-link to="/products"><router-link/>
+            <router-link to="/About"><router-link/>
+        </nav>
+    </header>
+</template>
