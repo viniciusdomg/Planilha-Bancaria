fd277a60841236a2c0c10862621fda6299178e71
Vinicius de Oliveira <viniciusmgomes2005@hotmail.com>
2024-05-23 17:43:38 -0300

    Adicionando o estilo para configurar cor de fundo do site

--- visao/financeiro/src/App.vue
+++ visao/financeiro/src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { RouterView } from 'vue-router'
+  import { RouterView } from 'vue-router';
+  import HeaderComponent from './components/Header.vue';
 </script>
 
-<template>
-  <router-view/>
+<template> 
+  <div class="app-container">
+    <HeaderComponent/>
+    <RouterView/>
+  </div>
 </template>
 
 <style scoped>
-
+  .app-container{
+    margin: 0;
+    background-color: #1E1E1E;
+    min-height: 100vh;
+    color: white;
+  }
 </style>
